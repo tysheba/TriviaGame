8b137c9d37db6da6a70aba1adc782c4e9386c01f
Tysheba <tmorgan2dev@gmail.com>
2018-07-30 17:25:34 -0400

    added images to the object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,30 +2,35 @@
 var gameQuestions = [
     {
         question: "What are the names of the original members of Destiny's Child?",
-        answers: ["Beyonce, Kelly Rowland, xxxx, and xxxx","Beyonce Kelly Rowland, xxx","Beyonce, Michelle Williams, xxx, xxx"
+        answers: ["Beyonce, Kelly Rowland, LeToya Luckett, and LaTavia Robertson","Beyonce, Kelly Rowland, and Michelle Williams","Beyonce, Michelle Williams, LaToya Luckett, and Kelly Rowland"
         ],
     
-        correctAnswer: 0
+        correctAnswer: 0,
+        image: "assets/images/1destinyschild.jpg"
     },
     {
         question: "What is the name of the first rap group to win a grammy award?",
         answers: ["Run D MC", "Salt-N Pepa", "Jazzy Jeff and the Fresh Prince" ],  
-        correctAnswer: 2
+        correctAnswer: 2,
+        image: "assets/images/2JazzyJeffnFreshPrince.jpg"
     },
     {
         question: "Phife Dawg, Q-Tip, and Ali Shaheed Muhammad were members of what rap group?",
         answers: [ "De La Soul", "A Tribe Called Quest", "Digital Underground"],
-        correctAnswer: 2
+        correctAnswer: 2, 
+        image: "assets/images/3tribeCalledQuest.png"
     },
     {
         question: "True or False. Lil Wayne, Juvenile and Mannie Fresh formed the 90's group the <em>Hot Boyz</em>.",
-        answers: ["True","False"],
-        correctAnswer: 1
+        answers: ["True","False, the Hot Boyz consisted of Lil Wayne, Juvenile, and BG"],
+        correctAnswer: 1,
+        image: "assets/images/4HotBoyz.jpg"
     },
     {
         question: "Who is the best selling rap artist of all time?",
         answers: ["Eminem", "Drake", "Tupac Shakur"],
-        correctAnswer: 0
+        correctAnswer: 0,
+        image: "assets/images/5Eminem.jpg"
     }
 ];
 var x = 0; //index for question
