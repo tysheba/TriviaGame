997f46dae54f66c6e94b82b5b5cf5f1a15da2f10
Tysheba <tmorgan2dev@gmail.com>
2018-07-31 23:25:09 -0400

    updated images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,31 +6,36 @@ var gameQuestions = [
         ],
     
         correctAnswer: 0,
-        image: "assets/images/1destinyschild.jpg"
+        image: "'assets/images/1destinyschild.jpg'",
+        text: "The correct answer is Beyonce, Kelly Rowland, LeToya Luckett, and LaTavia Robertson."
     },
     {
         question: "What is the name of the first rap group to win a grammy award?",
         answers: ["Run D MC", "Salt-N Pepa", "Jazzy Jeff and the Fresh Prince" ],  
         correctAnswer: 2,
-        image: "assets/images/2JazzyJeffnFreshPrince.jpg"
+        image: "'assets/images/2JazzyJeffnFreshPrince.jpg'",
+        text: "The correct answer is Jazzy Jeff and the Fresh Prince."
     },
     {
         question: "Phife Dawg, Q-Tip, and Ali Shaheed Muhammad were members of what rap group?",
         answers: [ "De La Soul", "A Tribe Called Quest", "Digital Underground"],
-        correctAnswer: 2, 
-        image: "assets/images/3tribeCalledQuest.png"
+        correctAnswer: 1, 
+        image: "'assets/images/3tribeCalledQuest.png'",
+        text: "The correct answer is A Tribe Called Quest."
     },
     {
         question: "True or False. Lil Wayne, Juvenile and Mannie Fresh formed the 90's group the <em>Hot Boyz</em>.",
-        answers: ["True","False, the Hot Boyz consisted of Lil Wayne, Juvenile, and BG"],
+        answers: ["True","False"],
         correctAnswer: 1,
-        image: "assets/images/4HotBoyz.jpg"
+        image: "'assets/images/4HotBoyz.jpg'",
+        text: "The correct answer is false.  The Hot Boyz consisted of Lil Wayne, Juvenile, and BG."
     },
     {
         question: "Who is the best selling rap artist of all time?",
         answers: ["Eminem", "Drake", "Tupac Shakur"],
         correctAnswer: 0,
-        image: "assets/images/5Eminem.jpg"
+        image: "'assets/images/5Eminem.jpg'",
+        text: "The correct answer is Eminem."
     }
 ];
 var x = 0; //index for question
@@ -39,17 +44,26 @@ var $questionDiv = $("#question");
 var $radioBtns = $("#radioBtns");
 var wrongAnswer = 0;
 timesUp = function () {
-    alert("Times up! the correct answer is " + gameQuestions[x].answers[correctNum]);
+    $(".images").html("<h4>" + gameQuestions[x].text + "</h4></br><p><img src =" + gameQuestions[x].image + "></p>")
+    // alert(gameQuestions[x].text);
     $questionDiv.empty();
     $radioBtns.empty();
+    // nextQ();
+    setTimeout(nextQ,3000);
+}
+function nextQ () {
     x++;
-    wrongAnswer++;
+    $(".images").empty();
     displayQuestion();
-
 }
 // var timer = setTimeout(timesUp, 15 * 1000);
 // Add function to display game questions
 var displayQuestion = function () {
+    if (x===5) {
+        $("#game").html("<h1> Game Over <h1>")
+    }
+    else {
+
     var choices = gameQuestions[x].answers;
     var triviaQuestion = "<h2> " + gameQuestions[x].question + "</h2>";
     $questionDiv.html(triviaQuestion);
@@ -65,5 +79,7 @@ var displayQuestion = function () {
      console.log(correctNum);
      console.log(gameQuestions[x].answers[correctNum]);
        
-}
-$(".btn").on("click", displayQuestion());
+} }
+
+// $(".btn").on("click", displayQuestion());
+$( document ).ready(displayQuestion);
